6bd8c5ba087e65f6738b9e533b6a8338583e4edf
tedga <tedga@DESKTOP-1JD4FJC>
2023-04-26 10:36:19 -0500

    Añadí el loggin, los service funcionan :3

--- Inventario-spring/src/main/java/com/inventario/core/InventarioSpringApplication.java
+++ Inventario-spring/src/main/java/com/inventario/core/InventarioSpringApplication.java
@@ -1,13 +1,28 @@
 package com.inventario.core;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.inventario.core.entities.Producto;
+import com.inventario.core.repositories.ProductoRepository;
 
 @SpringBootApplication
 public class InventarioSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventarioSpringApplication.class, args);
+		ApplicationContext context = SpringApplication.run(InventarioSpringApplication.class,args);
+		LocalDate localDate = LocalDate.now();
+		
+		ProductoRepository repository = context.getBean(ProductoRepository.class);
+		Producto sudadera = new Producto(null, "Sudadera", 350, new Date(26/04/2023), new Date(0), null, null, null, null);
+		
+		repository.save(sudadera);
+		System.out.println("El numero de productos en la base de datos es: "+repository.count());
+		System.out.println(repository.findAll());
 	}
 
 }
